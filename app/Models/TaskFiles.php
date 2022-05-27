74b84861469d5b208194498f39d38f1d2101e94c
Platinum <platinumemirate@gmail.com>
2022-05-27 16:25:29 +0100

    TasksFiles

---
+++ app/Models/TaskFiles.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaskFiles extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'task_id', 'file_name', 'file_type', 'url'
+    ];
+}
