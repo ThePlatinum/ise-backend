da441d7f8a95431d6490f0ba14ecc155dadd8c75
Platinum <platinumemirate@gmail.com>
2022-05-28 21:19:50 +0100

    Add phone event handle

--- app/Providers/SendPhoneVerificationNotification.php
+++ app/Providers/SendPhoneVerificationNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\Auth\VerifyPhoneController;
 use App\Providers\AddPhone;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
@@ -28,12 +29,8 @@ class SendPhoneVerificationNotification
     {
         //
         $user = $event->user;
-        // TODO: Send SMS with OTP
-        $otp = $this->generateOTP();
-    }
-
-    public function generateOTP(){
-        $otp = rand(100000, 999999);
-        return $otp;
+        
+        $verify = new VerifyPhoneController();
+        $verify->sendSMS($user);
     }
 }
