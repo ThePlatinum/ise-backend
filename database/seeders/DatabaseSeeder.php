002f3a9eecfd3922819fc0d474156c7ddd901b16
Platinum <platinumemirate@gmail.com>
2022-05-28 17:01:48 +0100

    seed frontend dev login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,10 +29,16 @@ class DatabaseSeeder extends Seeder
       'role' => '1',
     ]);
 
-    // Create a know user
+    // Create a known user
     User::create([
       'email'     => 'dprince195@gmail.com',
       'password'  => Hash::make('12345678'),
+      'email_verified_at' => now(),
+    ]);
+    // Frontend Engineer
+    User::create([
+      'email'     => 'quadratwemimo@gmail.com',
+      'password'  => '$2y$10$P6TWJkB/BQnunoW5S5YVm.SWIHkwOpb9.ZTuM1dgflO4vp795O6aG',
     ]);
 
     $catlist = ['Programming and Tech', 'Graphics Design', 'Copywriting', 'Photography, Video and Animations'];
