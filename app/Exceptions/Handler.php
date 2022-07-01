1df14ae52f2c2b8f9ae832a1bfc3d331ad82df5e
Platinum <platinumemirate@gmail.com>
2022-07-01 13:41:01 +0100

    TokenMismatchException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,39 +3,59 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<Throwable>>
-     */
-    protected $dontReport = [
-        //
-    ];
+  /**
+   * A list of the exception types that are not reported.
+   *
+   * @var array<int, class-string<Throwable>>
+   */
+  protected $dontReport = [
+    //
+  ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+  /**
+   * A list of the inputs that are never flashed for validation exceptions.
+   *
+   * @var array<int, string>
+   */
+  protected $dontFlash = [
+    'current_password',
+    'password',
+    'password_confirmation',
+  ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+  /**
+   * Register the exception handling callbacks for the application.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->reportable(function (Throwable $e) {
+      //
+    });
+  }
+
+  public function render($request, Throwable $exception)
+  {
+    if ($exception instanceof TokenMismatchException) {
+      if ($request->getRequestUri() === '/logout') {
+        auth()->logout();
+        return redirect()->route('login');
+      } else {
+        return redirect()
+          ->back()
+          ->withInput($request->except('password'))
+          ->with([
+            'status' => 'Oops! Session expired. Please try again',
+            'alert' => 'danger'
+          ]);
+      }
     }
+    return parent::render($request, $exception);
+  }
 }
